3a6dbbf4376f503f9969c351f71a2f1c3c724450
Kang-Kyu Lee <klee@aspiration.com>
2022-03-22 23:57:52 -0700

    Add stimulus form

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+
+  static targets = ["email"]
+
+  submit(e) {
+    e.preventDefault()
+
+    fetch("http://localhost:3001/users", {
+      method: 'POST',
+      mode: 'cors',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        user: {
+          email: this.emailTarget.value
+        }
+      })
+    }).then(response => {
+      console.log(response)
+    })
   }
 }
